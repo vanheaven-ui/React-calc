6649bb2656d7de76363ddebbfc87554907403715
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-18 16:26:56 +0300

    Add test JSX react element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App"></div>;
+  const myText = 'I have completed the project setup. Time to get building... Well I should remember to lint the code.';
+  return (
+    <div className="App" style={{ color: 'red', fontSize: 64 }}>
+      {myText}
+    </div>
+  );
 }
 
 export default App;
