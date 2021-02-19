7cc73ce7c6f1737bd4ab849c4790c60161179a63
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-19 16:15:16 +0300

    Pass color and wide as props

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 const Button = ({ ButtonName, color, wide }) => (
   <button
     type="button"
-    className={color + (wide ? ' double-width-btn' : ' default-btn-width')}
   >
     {ButtonName}
   </button>
