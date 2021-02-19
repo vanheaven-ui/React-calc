b188739f70f0945027c1cbb03f31d9d13f0d9ef4
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-19 22:39:49 +0300

    Update the jsx element

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,18 +1,7 @@
 import PropTypes from 'prop-types';
 
 const Display = ({ output }) => (
-  <div
-    className="display-window"
-    style={{
-      border: '3px solid red',
-      width: 'max-content',
-      margin: '0 auto',
-      padding: '2px 10px',
-      marginBottom: '10px',
-    }}
-  >
-    Output is:
-    {' '}
+  <div className="display-window">
     {output}
   </div>
 );
