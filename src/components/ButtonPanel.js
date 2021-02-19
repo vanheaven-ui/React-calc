bef98ba53bde7e25754e871ac3b34e3deb827bfd
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-19 12:12:06 +0300

    Implement the ButtonPanel Component

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,42 @@
+import Button from './Button';
+
+const ButtonPanel = () => {
+  const renderButton = name => (
+    <Button ButtonName={name} />
+  );
+  return (
+    <div className="btn-panel">
+      <div className="btn-group">
+        { renderButton('AC')}
+        { renderButton('+/-')}
+        { renderButton('%')}
+        { renderButton('/')}
+      </div>
+      <div className="btn-group">
+        { renderButton('7')}
+        { renderButton('8')}
+        { renderButton('9')}
+        { renderButton('X')}
+      </div>
+      <div className="btn-group">
+        { renderButton('4')}
+        { renderButton('5')}
+        { renderButton('6')}
+        { renderButton('-')}
+      </div>
+      <div className="btn-group">
+        { renderButton('1')}
+        { renderButton('2')}
+        { renderButton('3')}
+        { renderButton('+')}
+      </div>
+      <div className="btn-group">
+        { renderButton('0')}
+        { renderButton('.')}
+        { renderButton('=')}
+      </div>
+    </div>
+  );
+};
+
+export default ButtonPanel;
