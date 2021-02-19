78d79f55555bbc06035ebba57d8563fec2b0b2f8
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-19 10:58:18 +0300

    Update src folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ButtonPanel from "./components/Buttonpanel";
+
 function App() {
-  const myText = 'I have completed the project setup. Time to get building... Well I should remember to lint the code.';
+  const myText =
+    "I have completed the project setup. Time to get building... Well I should remember to lint the code.";
   return (
-    <div className="App" style={{ color: 'red', fontSize: 64 }}>
+    <div className="App" style={{ color: "red", fontSize: 64 }}>
       {myText}
     </div>
   );
