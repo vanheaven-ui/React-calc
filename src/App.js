25ca521476a25872ab15e6502bb74505e9db1d5e
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-19 11:01:10 +0300

    Resolve eslint offenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import ButtonPanel from "./components/Buttonpanel";
-
 function App() {
-  const myText =
-    "I have completed the project setup. Time to get building... Well I should remember to lint the code.";
+  const myText = 'I have completed the project setup. Time to get building... Well I should remember to lint the code.';
   return (
-    <div className="App" style={{ color: "red", fontSize: 64 }}>
+    <div className="App" style={{ color: 'red', fontSize: 64 }}>
       {myText}
     </div>
   );
