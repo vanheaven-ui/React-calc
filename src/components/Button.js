1f671b41b78c6d21b7aa4d4c90c5b8787bc7f149
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-20 14:04:56 +0300

    Pass onClick prop from ButtonPanel component
    Update the Proptypes validation accordingly

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ ButtonName, color, wide }) => (
+const Button = ({
+  ButtonName, color, wide, onClick,
+}) => (
   <button
     type="button"
     className={color + (wide ? ' double-btn' : ' default-btn')}
+    onClick={() => onClick(ButtonName)}
   >
     {ButtonName}
   </button>
@@ -19,6 +22,7 @@ Button.propTypes = {
   ButtonName: PropTypes.string,
   color: PropTypes.string,
   wide: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Button;
