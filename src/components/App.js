c4a65b8b6fb9f60f0ae737ab84384966f03f0499
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-20 12:48:43 +0300

    Translate App sfc to a stateful component
    Define hancleClick methid

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,57 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-      <button
-        type="button"
-        onClick={() => {
-          calculate({ total: 1, next: 0, operation: '+' }, '=');
-          const display = document.querySelector('.display-window');
-          const div = document.createElement('div');
-          div.setAttribute('style', 'font-size: 24px; width: max-content; margin: 0 auto; color: green;');
-          div.innerHTML = 'Just a placeholder!';
-          display.insertAdjacentElement('beforebegin', div);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-          setTimeout(() => {
-            div.remove();
-          }, 2000);
-        }}
-      >
-        PlaceHolder Btn
-      </button>
-    </>
-  );
+  handleClick(buttonName) {
+    calculate(this.state, buttonName);
+  }
+
+  render () {
+    return (
+      <>
+        <Display />
+        <ButtonPanel handleClick={this.handleClick}/>
+      </>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <>
+//       <Display />
+//       <ButtonPanel />
+//       <button
+//         type="button"
+//         onClick={() => {
+//           calculate({ total: 1, next: 0, operation: '+' }, '=');
+//           const display = document.querySelector('.display-window');
+//           const div = document.createElement('div');
+//           div.setAttribute('style', 'font-size: 24px; width: max-content; margin: 0 auto; color: green;');
+//           div.innerHTML = 'Just a placeholder!';
+//           display.insertAdjacentElement('beforebegin', div);
+
+//           setTimeout(() => {
+//             div.remove();
+//           }, 2000);
+//         }}
+//       >
+//         PlaceHolder Btn
+//       </button>
+//     </>
+//   );
+// }
 
 export default App;
