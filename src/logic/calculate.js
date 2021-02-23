ea052364601af7b139c8834967e759159acc94d6
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-23 05:51:27 +0300

    Add validations for proper functionality

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,6 +2,7 @@ import operate from './operate';
 
 const calculate = (dataObj, buttonName) => {
   let { total, next, operation } = dataObj;
+  console.log(total, next);
   switch (buttonName) {
     case '+/-':
       if (total && !next) {
@@ -17,24 +18,25 @@ const calculate = (dataObj, buttonName) => {
       break;
     case '%':
       if (total && !next) {
-        total = operate(total, 100, '÷');
-        total.toString();
+        total /= 100;
       } else if (next) {
-        next = operate('0.01', next, 'x');
-        next.toString();
+        next /= 100;
       }
       break;
     default:
       break;
   }
 
-  if (buttonName === '=') {
-    total = operate(parseInt(total, 10), parseInt(next, 10), operation);
+  if (buttonName === '=' && total && next) {
+    total = operate(total, next, operation);
+    console.log(typeof (total));
     if (typeof (total) === 'string') {
       total = null;
       const alert = document.createElement('div');
       alert.innerHTML = 'Cannot divide by 0!';
-      alert.setAttribute('style', 'background-color: rgba(255, 68, 34, 0.3; max-width: 700px; margin: 0 auto;');
+      alert.setAttribute(
+        'style', 'background-color: rgba(255, 68, 34, 0.3; max-width: 700px; margin: 0 auto;',
+      );
       alert.classList.add('alert');
       document.body.insertAdjacentElement('afterBegin', alert);
       setTimeout(() => {
