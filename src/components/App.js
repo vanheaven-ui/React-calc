743cbf49c7871faecef193749e82a883530f1b52
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-02-23 05:50:26 +0300

    Refactor handleClick method

--- src/components/App.js
+++ src/components/App.js
@@ -19,19 +19,22 @@ class App extends React.Component {
   handleClick(buttonName) {
     const stateCopy = { ...this.state };
     const dataObjCopy = stateCopy.dataObj;
-    const mutants = ['+/-', '%', '', ''];
+    const mutants = ['+/-', '%'];
 
-    console.log((Number.isNaN(parseInt(buttonName, 10))));
-
-    if (dataObjCopy.total === null && buttonName === '.') {
-      dataObjCopy.total = '0';
-    }
-
+    // This guy gives us the non number buttons
     if (Number.isNaN(parseInt(buttonName, 10)) && buttonName !== '=' && buttonName !== '.') {
       if (!mutants.includes(buttonName)) {
+        // This if block checks if the dataObjCopy ends with a . and avoids adding more ...
+        // if (dataObjCopy.total.toString().endsWith('.', dataObjCopy.total.toString().length)) {
+        //   dataObjCopy.operation = null;
+        // } else {
         dataObjCopy.operation = buttonName;
+        // }
       }
-    } else if (typeof (parseInt(buttonName, 10)) === 'number' && buttonName !== '=') {
+      if (dataObjCopy.total === null) {
+        dataObjCopy.total = '0';
+      }
+    } else if (typeof (parseInt(buttonName, 10)) === 'number' && buttonName !== '=') { // this gives me the number buttons
       if (dataObjCopy.operation !== null) {
         if (dataObjCopy.next === null) {
           dataObjCopy.next = buttonName;
@@ -41,11 +44,16 @@ class App extends React.Component {
       } else if (dataObjCopy.total === null) {
         dataObjCopy.total = buttonName;
       } else if (dataObjCopy.total !== null) {
-        dataObjCopy.total += buttonName;
+        /* if you have a result that ia a number, this avoids concatenating
+        numbers to value before appending an operation */
+        if (typeof (dataObjCopy.total) === 'object') {
+          dataObjCopy.total = buttonName;
+        } else {
+          dataObjCopy.total += buttonName;
+        }
       }
     }
     this.setState({ dataObj: calculate(dataObjCopy, buttonName) });
-    console.log(dataObjCopy, buttonName);
   }
 
   render() {
@@ -53,6 +61,7 @@ class App extends React.Component {
     const nxt = dataObj.next;
     const opr = dataObj.operation;
     console.log(dataObj.total, nxt, opr);
+    console.log(dataObj);
     return (
       <>
         <Display
